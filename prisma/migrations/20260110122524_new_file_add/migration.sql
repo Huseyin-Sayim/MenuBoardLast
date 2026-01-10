65a4937be0d56003e33aca6e0ee8f9b5bd37634d
Hüseyin SAYIM <129699065+Huseyin-Sayim@users.noreply.github.com>
2026-01-11 02:07:25 +0300

    delete functions

--- prisma/migrations/20260110122524_new_file_add/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,templateId]` on the table `TemplateConfig` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "TemplateConfig_userId_templateId_key" ON "TemplateConfig"("userId", "templateId");
